1dad6ca1099215563dcb87b5e252c7bf3bdead0f
Erkan <erkansoftwaredeveloper@gmail.com>
2026-06-21 21:20:37 +0200

    FEAT: FastAPI with CRUD operations

--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ from app.database import SessionLocal, engine
 app = FastAPI()
 
 
+# DB SESSION DEPENDENCY
+
 def get_db():
     db = SessionLocal()
     try:
@@ -19,3 +21,51 @@ def get_db():
 @app.get("/")
 def root():
     return {"message": "Finance Manager API is running"}
+
+
+# ----------- USERS -------------------
+@app.post("/register")
+def register_user(username: str, password: str, db: Session = Depends(get_db)):
+    # add new user to the database
+    user = crud.create_user(db, username, password)
+
+    return {
+        "message": "User created",
+        "user_id": user.user.id
+    }
+
+
+# ------------ USER get BY ID-------------------
+@app.get("/users/{user_id}")
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = crud.get_user(db, user_id)
+    return user
+
+
+# ----------- TRANSACTIONS -------------------
+@app.post("/transactions")
+def create_transaction(user_id: int, amount: float, category: str, db: Session = Depends(get_db)):
+    transaction = crud.create_transaction(db, user_id, amount, category)
+    return {
+        "message": "Transaction added",
+        "transaction_id": transaction.transaction_id
+    }
+
+# ----------- TRANSACTIONS List -------------------
+
+
+@app.get("/transactions/{user_id}")
+def get_transactions(user_id: int, db: Session = Depends(get_db)):
+    return crud.get_transactions(db, user_id)
+
+# ----------- BALANCE -------------------
+
+
+@app.get("/balance/{user_id}")
+def get_balance(user_id: int, db: Session = Depends(get_db)):
+    balance = crud.get_balance(db, user_id)
+
+    return {
+        "user_id": user_id,
+        "balance": balance
+    }
