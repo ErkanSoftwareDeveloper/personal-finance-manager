af4895959a6315b8350da5d0dfd39b5460d960e2
Erkan <erkansoftwaredeveloper@gmail.com>
2026-07-19 19:32:13 +0300

    FIX: hande duplicate usernames

--- app/main.py
+++ app/main.py
@@ -69,7 +69,14 @@ def root():
 # ----------- USERS -------------------
 @app.post("/register", response_model=UserResponse)
 def register_user(user_data: UserRegister, db: Session = Depends(get_db)):
-    # add new user to the database
+    existing_user = crud.get_user_by_username(db, user_data.username)
+
+    if existing_user is not None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username already exists"
+        )
+
     user = crud.create_user(
         db=db,
         username=user_data.username,
